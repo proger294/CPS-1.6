b7f9cd74d50001ef182b75b6d52cc9d5074f3320
Timur Elsukov <rasputin.t@list.ru>
2024-09-01 00:49:20 +0300

    change vibrate

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,7 +1,7 @@
-const buttons = document.querySelectorAll(".btn-circle, .repair-btn, .btn-order");
+const buttons = document.querySelectorAll(".btn-circle");
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        navigator.vibrate(10);
+        navigator.vibrate(5);
     });
   });
